bf90580ad3206ad31d5ed9dfc390048a1599957d
abaiA <abai.45.abai@gmail.com>
2024-06-28 20:08:30 +0500

    Adding goods

--- src/main/java/kz/abai/eCommerce/service/impl/GoodsServiceImpl.java
+++ src/main/java/kz/abai/eCommerce/service/impl/GoodsServiceImpl.java
@@ -1,8 +1,33 @@
 package kz.abai.eCommerce.service.impl;
 
+import kz.abai.eCommerce.dto.GoodDto;
+import kz.abai.eCommerce.entities.CategoryEntity;
+import kz.abai.eCommerce.mapper.GoodsMapper;
+import kz.abai.eCommerce.repository.CategoryRepository;
+import kz.abai.eCommerce.repository.GoodsRepository;
 import kz.abai.eCommerce.service.GoodsService;
+import kz.abai.eCommerce.utils.GoodsUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
+@RequiredArgsConstructor
 public class GoodsServiceImpl implements GoodsService {
+    private final GoodsRepository goodsRepository;
+    private final GoodsUtils goodsUtils;
+    private final CategoryRepository categoryRepository;
+    private final GoodsMapper goodsMapper;
+    @Override
+    public GoodDto addGood(String name, String description, String imgUrl, String categoryName, BigDecimal cost) {
+        var categoryEntity = getCategoryByName(categoryName);
+        var goodEntity = goodsUtils.addNewGoodEntity(name, description, imgUrl, categoryEntity, cost);
+        return goodsMapper.toDto(goodEntity);
+    }
+
+    private CategoryEntity getCategoryByName(String categoryName) {
+        return categoryRepository.findByNameIgnoreCase(categoryName)
+                .orElseThrow(() -> new RuntimeException("Category by name not found"));
+    }
 }
---
+++ src/main/java/kz/abai/eCommerce/utils/GoodsUtils.java
@@ -0,0 +1,23 @@
+package kz.abai.eCommerce.utils;
+
+import kz.abai.eCommerce.dto.GoodDto;
+import kz.abai.eCommerce.entities.CategoryEntity;
+import kz.abai.eCommerce.entities.GoodsEntity;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Component
+public class GoodsUtils {
+    public GoodsEntity addNewGoodEntity(String name, String description, String imgUrl, CategoryEntity category, BigDecimal cost) {
+        return GoodsEntity.builder()
+                .goodId(UUID.randomUUID().toString())
+                .name(name)
+                .description(description)
+                .imgUrl(imgUrl)
+                .category(category)
+                .cost(cost)
+                .build();
+    }
+}
