b75540594169b664d3f81bfc6ed9a671ea090345
abaiA <abai.45.abai@gmail.com>
2024-06-28 22:07:13 +0500

    Added default photo for goods

--- src/main/java/kz/abai/eCommerce/service/impl/GoodsServiceImpl.java
+++ src/main/java/kz/abai/eCommerce/service/impl/GoodsServiceImpl.java
@@ -11,18 +11,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class GoodsServiceImpl implements GoodsService {
-    private final GoodsRepository goodsRepository;
-    private final GoodsUtils goodsUtils;
-    private final CategoryRepository categoryRepository;
-    private final GoodsMapper goodsMapper;
+    private static final String DEFAULT_IMG_URL = "https://cdn.vectorstock.com/i/500p/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg";
+
+    private GoodsRepository goodsRepository;
+    private GoodsUtils goodsUtils;
+    private CategoryRepository categoryRepository;
+    private GoodsMapper goodsMapper;
+
+    public GoodsServiceImpl(GoodsRepository goodsRepository, GoodsUtils goodsUtils, CategoryRepository categoryRepository, GoodsMapper goodsMapper) {
+        this.goodsRepository = goodsRepository;
+        this.goodsUtils = goodsUtils;
+        this.categoryRepository = categoryRepository;
+        this.goodsMapper = goodsMapper;
+    }
     @Override
     public GoodDto addGood(String name, String description, String imgUrl, String categoryName, BigDecimal cost) {
         var categoryEntity = getCategoryByName(categoryName);
-        var goodEntity = goodsUtils.addNewGoodEntity(name, description, imgUrl, categoryEntity, cost);
+        imgUrl = Optional.ofNullable(imgUrl).filter(url -> !url.isEmpty()).orElse(DEFAULT_IMG_URL);
+        
+        var goodEntity = goodsRepository.save(goodsUtils.addNewGoodEntity(name, description, imgUrl, categoryEntity, cost));
         return goodsMapper.toDto(goodEntity);
     }
 
