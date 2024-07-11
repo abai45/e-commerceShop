ebf82e2df68cf9a1628408d1fabdbfd3a83a851d
abaiA <abai.45.abai@gmail.com>
2024-07-11 22:26:19 +0500

    add attribute values to product/goods

---
+++ src/main/java/kz/abai/eCommerce/entities/AttributeValueEntity.java
@@ -0,0 +1,26 @@
+package kz.abai.eCommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "attribute_values")
+public class AttributeValueEntity extends Auditable{
+    @Column(nullable = false)
+    private String nameKZ;
+    @Column(nullable = false)
+    private String nameRU;
+    @Column(nullable = false)
+    private String nameEN;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attribute_id")
+    private AttributeEntity attribute;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id")
+    private GoodsEntity good;
+}
