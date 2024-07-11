e9e0f7a3e2c96a20c55cffdcbd1a6ef44fb14bfd
abaiA <abai.45.abai@gmail.com>
2024-07-11 22:28:47 +0500

    add attribute values to categories

--- src/main/java/kz/abai/eCommerce/entities/CategoryAttributeEntity.java
+++ src/main/java/kz/abai/eCommerce/entities/CategoryAttributeEntity.java
@@ -1,4 +1,20 @@
 package kz.abai.eCommerce.entities;
 
-public class CategoryAttributeEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category_attributes")
+public class CategoryAttributeEntity extends Auditable {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attribute_id")
+    private AttributeEntity attribute;
 }
